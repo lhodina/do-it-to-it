50046a8d7048c37a925bbfc797a07948b8bfb309
lhodina <lhodina@gmail.com>
2023-09-18 12:32:22 -0500

    MERGE CONFLICT TIME TO FIGHT

--- test_working_branch_update.py
+++ test_working_branch_update.py
@@ -1,4 +1,4 @@
 print("Figure out how updating the working branch works")
 print("I think this works, as long as I don't directly change an existing line?")
 print("This is a line written in luke-branch")
-print("This line is written in main")
+print("I'M TAKING OVER LINE 4 YOUR CODE SUCKS I AM A GOD MERGE CONFLICT MERGE CONFLICT")

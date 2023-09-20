cc65abd3c98caa7791cb0879e3c6af9888ea6118
Jason Grammergit config --global user.name Jason <grammerjasonl@gmail.com>
2023-09-20 11:17:35 -0500

    changes

--- flask_app/controllers/priorities.py
+++ flask_app/controllers/priorities.py
@@ -1,8 +1,8 @@
 
 from flask import render_template, redirect, request, session
-
 from flask_app import app
 from flask_app.models import priority
+from flask_app.models import user
 
 
 @app.route("/priorities", methods=["POST"])
@@ -13,9 +13,32 @@ def add_priority():
         "level": request.form["level"]
     }
     # VALIDATIONS COMING SOON:
-    # session["data"] = data
-    # if not priority.Priority.validate_priority(data):
-    #     return redirect("/priorities/new")
-    # session.pop("data")
+    session["data"] = data
+    if not priority.Priority.validate_priority(data):
+        return redirect("/dashboard")
+    session.pop("data")
     priority.Priority.save(data)
     return redirect("/dashboard")
+
+@app.route('/priority/edit/<int:id>', methods=['POST'])
+def process_edit_priority(id):
+    if 'user_id' not in session:
+        return redirect('/')
+    if not priority.Priority.validate_priority(request.form):
+        return redirect(f'/priority/edit/{id}')
+
+    data = {
+        'id': id,
+        'text': request.form['text'],
+        'level': request.form['level'],
+    }
+    priority.Priority.update(data)
+    return redirect('/dashboard')
+
+@app.route('/priority/destroy/<int:id>')
+def destroy_priority(id):
+    if 'user_id' not in session:
+        return redirect('/')
+
+    priority.Priority.destroy({'id':id})
+    return redirect('/dashboard')
--- flask_app/models/priority.py
+++ flask_app/models/priority.py
@@ -1,4 +1,6 @@
 from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+from flask_app.models import user
 
 class Priority:
     DB = "do_it_to_it"
@@ -9,6 +11,7 @@ class Priority:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
         self.user_id = data['user_id']
+        self.creator = None
 
     @classmethod
     def save(cls, data):
@@ -17,6 +20,31 @@ class Priority:
         VALUES ( %(text)s, %(level)s, %(user_id)s)
         """
         return connectToMySQL(cls.DB).query_db(query, data)
+    
+    @classmethod
+    def get_by_id(cls,data):
+        query = """
+                SELECT * FROM priorities
+                JOIN users on priorities.user_id = users.id
+                WHERE priorities.id = %(id)s;
+                """
+        result = connectToMySQL(cls.db).query_db(query,data)
+        if not result:
+            return False
+
+        result = result[0]
+        this_priority = cls(result)
+        user_data = {
+                "id": result['users.id'],
+                "first_name": result['first_name'],
+                "last_name": result['last_name'],
+                "email": result['email'],
+                "password": "",
+                "created_at": result['users.created_at'],
+                "updated_at": result['users.updated_at']
+        }
+        this_priority.creator = user.User(user_data)
+        return this_priority
 
     @classmethod
     def get_all_user_priorities(cls, data):
@@ -48,3 +76,15 @@ class Priority:
                 WHERE id = %(id)s;
                 """
         return connectToMySQL(cls.DB).query_db(query,data)
+    
+    @staticmethod
+    def validate_priority(form_data):
+        is_valid = True
+
+        if len(form_data['text']) < 2:
+            flash("Description must be at least 2 characters long.")
+            is_valid = False
+        if len(form_data['level']) < 1:
+            flash("must have priority level.")
+            is_valid = False
+        return is_valid

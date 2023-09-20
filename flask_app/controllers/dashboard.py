095e7957776e3c4535ae93d5bf22d2dd4fe31b96
lhodina <lhodina@gmail.com>
2023-09-19 20:59:17 -0500

    Update models and controllers to feed data to Tag page

--- flask_app/controllers/dashboard.py
+++ flask_app/controllers/dashboard.py
@@ -1,17 +1,27 @@
-from flask import render_template
+from flask import render_template, redirect, request, session
 
 from flask_app import app
-from flask_app.models import category, tag, priority
+from flask_app.models import category, tag, priority, link
 
 
 @app.route("/dashboard")
 def go_to_dashboard():
-    all_priorities = priority.Priority.get_all_priorities()
-    all_categories = category.Category.get_all_categories()
-    all_tags = tag.Tag.get_all_tags()
-    print("***** all_categories: ", all_categories)
-    print()
-    print("***** all_tags: ", all_tags)
-    print()
-    print("***** all_priorities: ", all_priorities)
-    return render_template("dashboard.html", all_priorities=all_priorities, all_categories=all_categories, all_tags=all_tags)
+    if session and session["current_user"]:
+        data = {
+            "id": session["current_user"]["id"]
+        }
+        print("data: ", data)
+        all_user_priorities = priority.Priority.get_all_user_priorities(data)
+        all_user_links = link.Link.get_all_user_links(data)
+        all_categories = category.Category.get_all_categories()
+        all_tags = tag.Tag.get_all_tags()
+        print("***** all_categories: ", all_categories)
+        print()
+        print("***** all_tags: ", all_tags)
+        print()
+        print("***** all_user_priorities: ", all_user_priorities)
+        print()
+        print("***** all_user_links: ", all_user_links)
+        return render_template("dashboard.html", all_user_priorities=all_user_priorities, all_categories=all_categories, all_tags=all_tags, all_user_links=all_user_links)
+    else:
+        return redirect("/")
